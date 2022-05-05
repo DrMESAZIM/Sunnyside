b70b7324339cdab3cfaefe4206f68d5b268c7e4a
DrMESAZIM <47475734+DrMESAZIM@users.noreply.github.com>
2022-05-05 08:56:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const menu =document.querySelector('.menu');
+const navigation= document.querySelector('.navigation');
+
+menu.addEventListener('click',function () {
+
+	// body...
+
+	navigation.classList.toggle('active');
+})
